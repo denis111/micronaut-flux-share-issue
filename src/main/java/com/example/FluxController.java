d59f868de17dfb3344c911b6f93d21186e9a2b37
Denis Kalgushkin <d.kalgushkin@tranxfer.com>
2021-10-13 17:28:23 +0200

    Copy flux to new one

--- src/main/java/com/example/FluxController.java
+++ src/main/java/com/example/FluxController.java
@@ -15,13 +15,19 @@ public class FluxController {
   @Put(consumes = MediaType.ALL)
   @Produces(MediaType.APPLICATION_JSON)
   public Mono<String> upload(@Body final Flux<byte[]> content) {
-    return content.shareNext()
+    final var newFlux = Flux.<byte[]>create(fluxSink ->
+            content.subscribe(fluxSink::next, fluxSink::error, fluxSink::complete))
+        .publish(1)
+        .autoConnect();
+
+    return newFlux.shareNext()
         .map(bytes -> {
           System.out.println("First bytes: " + new String(bytes, StandardCharsets.UTF_8));
-          return "ok";
-        }).flatMap(o -> content.reduce(System.out, (printStream, bytes) -> {
-          printStream.println("bytes: " + new String(bytes, StandardCharsets.UTF_8));
+          return Mono.just(bytes);
+        }).flatMap(o -> Flux.merge(o, newFlux).reduce(System.out, (printStream, bytes) -> {
+          printStream.println("bytes: " + bytes.length);
           return printStream;
-        }).then(Mono.just("ok")));
+        })).then(Mono.just("ok"));
   }
+
 }
